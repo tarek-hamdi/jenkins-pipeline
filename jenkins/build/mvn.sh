9b1e7c9f4837f205be8a4374e57620c949f53ff1
tarek-hamdi <trk_hamdi@yahoo.com>
2024-07-17 19:57:48 +0300

    Adjusting path

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
-WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/var/jenkins_home/workspace/pipeline-maven
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
